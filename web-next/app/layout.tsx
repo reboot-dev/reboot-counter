e35cfff145d6dc45f063c8657f768b5a0735573c
Rebot <96078724+reboot-dev-bot@users.noreply.github.com>
2024-11-28 10:20:57 +0000

    [Release] Synchronize for release

--- web-next/app/layout.tsx
+++ web-next/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import RebootContext from "./RebootContext.js";
+import RebootContext from "./RebootContext";
 
 export const metadata: Metadata = {
   title: "Reboot Counter",
