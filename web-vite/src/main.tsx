6d6d6267d078b2f54d4700c3937baace444a9d2b
Rebot <96078724+reboot-dev-bot@users.noreply.github.com>
2024-12-20 01:02:42 +0100

    [Release] Synchronize for release (#13)

--- web-vite/src/main.tsx
+++ web-vite/src/main.tsx
@@ -1,22 +1,12 @@
-import { RebootClient, RebootClientProvider } from "@reboot-dev/reboot-react";
+import { RebootClientProvider } from "@reboot-dev/reboot-react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
-function root() {
-  if (import.meta.env.VITE_PUBLIC_ENDPOINT === undefined) {
-    return <>Please set 'VITE_PUBLIC_ENDPOINT' in the '.env' file</>;
-  }
-
-  const client = new RebootClient(import.meta.env.VITE_PUBLIC_ENDPOINT);
-
-  return (
-    <React.StrictMode>
-      <RebootClientProvider client={client}>
-        <App />
-      </RebootClientProvider>
-    </React.StrictMode>
-  );
-}
-
-ReactDOM.createRoot(document.getElementById("root")!).render(root());
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RebootClientProvider url={"http://localhost:9991"}>
+      <App />
+    </RebootClientProvider>
+  </React.StrictMode>
+);
